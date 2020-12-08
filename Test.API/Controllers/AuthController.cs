e4e4161e3cec6a03eba5297d635e97868efd9ebc
ibrahim-islam <ibrahim.islam@live.com>
2020-12-09 01:24:58 +0700

    init

---
+++ Test.API/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Test.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("a72ae55b-7b31-4393-9764-a4ea5501290c");
+    }
+}
---
+++ Test.API/Controllers/DataController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+
+namespace Test.API.Controllers
+{
+    public class Request
+    {
+        public string Token { get; set; }
+        public string AnyData { get; set; }
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class DataController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post(Request request)
+        {
+            //check auth token
+            if (string.IsNullOrWhiteSpace(request?.Token) || request?.Token != "a72ae55b-7b31-4393-9764-a4ea5501290c") return Unauthorized();
+
+            //check data
+            if (string.IsNullOrWhiteSpace(request?.AnyData)) return BadRequest();
+
+            //produce message into rabbitmq
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+            var body = Encoding.UTF8.GetBytes(request.AnyData);
+            channel.BasicPublish(exchange: "logs",
+                                 routingKey: "",
+                                 basicProperties: null,
+                                 body: body);
+
+            return Accepted();
+        }
+    }
+
+}
